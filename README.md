d6af7bd9fc5aeffc15388fff4b1eadda42722c07
bideeen <b.abideen@yahoo.com>
2020-12-13 17:05:33 -0700

    Last modification

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 ## Introduction
 I built a simple web scraper that will return the content of cnn travel news article when given a specific URL. Some examples of real products which use similar technologies include price-tracking websites and SEO audit tools which may scrape top search results. This project may took me around 4 hours to complete.
 
+## How To Run The Script
+Simple as it is, just run the following codes:
+  - python cnn.py
 
+  That's all you need to do, and the file get saved on the cnn travel news folder.
 
 ## Libraries
 The python libraries used where:
@@ -15,3 +19,12 @@ The python libraries used where:
 ## Output
 The articles where stored in .txt format
 
+## Note
+This code only applies to CNN travel news alone. Any links used will not be scraped.
+
+## Further Works
+I intend on improving the code to perform the following:
+  - Scrape all news link
+  - Scrape each news per link
+  - Deploy the script using Flask
+  - Extend the Script functionality to Scrape other form of News on CNN
--- cnn.py
+++ cnn.py
@@ -1,24 +1,36 @@
-import requests
-from bs4 import BeautifulSoup
+# Importing the libraries to be used
+import requests                             # The request libraries used to connect to the site
+from bs4 import BeautifulSoup               # This library is used to scrape the data from the link from the data gotten from the request library
 
 
+# The Scrape funcitons that performs the scraping
 def scrape(url):
+    # The page
     page = requests.get(url)
+    # The page  content is gotten here
     soup = BeautifulSoup(page.content, 'html.parser')
+    # The title of the article is gotten here
     title = soup.find('title').text
-    # Article file
+    # Open an new article file for each link
     article = open(f'cnn travel news/{title}.txt', 'w')
+    # write the article topic
     article.write('*********************Article Topic********************************\n\n')
+    # write the article title in the new file
     article.write(title)
     article.write('\n\n\n*********************Article Content********************************\n\n')
+    # Fetch the content from the data gotten from the site
     paragraphs = soup.find_all('div', class_='Paragraph__component')
+    # iterate through the reault and write the each into the new file
     for i in paragraphs:
         article.write(i.text)
         article.write('\n')
     article.close
+    # return the article
     return article
 
+# The main function
 def main():
+    # it promts the user to input the site link
     try:
         site = str(input('Enter the Cnn Artcle to Scrape:  '))
         scrape(site)
